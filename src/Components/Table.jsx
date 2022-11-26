6fb28a46e9554f8443f60cb77a52099ed014818d
dev-himanshu-01 <himanshu01.dev@gmail.com>
2022-11-25 22:24:54 -0800

    elements added till 42!

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './Table.css'
+import React from "react";
+import "./Table.css";
 
 const Table = () => {
     return (
-        <section className='container'>
-            <div className="item">item_1</div>
+        <section className="container">
+            <div className="item">
+                <div className="atomicNumber">1</div>
+                <div className="elementSym">H</div>
+                <div className="elementName">Hydrogen</div>
+            </div>
+
             <div className="item">item_2</div>
             <div className="item">item_3</div>
             <div className="item">item_4</div>
@@ -22,9 +27,25 @@ const Table = () => {
             <div className="item">item_16</div>
             <div className="item">item_17</div>
             <div className="item">item_18</div>
-            <div className="item">item_19</div>
-            <div className="item">item_20</div>
-            <div className="item">item_21</div>
+
+            <div className="item">
+                <div className="atomicNumber">2</div>
+                <div className="elementSym">He</div>
+                <div className="elementName">Helium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">3</div>
+                <div className="elementSym">Li</div>
+                <div className="elementName">Lithium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">4</div>
+                <div className="elementSym">Be</div>
+                <div className="elementName">Beryllium</div>
+            </div>
+
             <div className="item">item_22</div>
             <div className="item">item_23</div>
             <div className="item">item_24</div>
@@ -36,14 +57,55 @@ const Table = () => {
             <div className="item">item_30</div>
             <div className="item">item_31</div>
             <div className="item">item_32</div>
-            <div className="item">item_33</div>
-            <div className="item">item_34</div>
-            <div className="item">item_35</div>
-            <div className="item">item_36</div>
-            <div className="item">item_37</div>
-            <div className="item">item_38</div>
-            <div className="item">item_39</div>
-            <div className="item">item_40</div>
+
+            <div className="item">
+                <div className="atomicNumber">5</div>
+                <div className="elementSym">B</div>
+                <div className="elementName">Boron</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">6</div>
+                <div className="elementSym">C</div>
+                <div className="elementName">Carbon</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">7</div>
+                <div className="elementSym">N</div>
+                <div className="elementName">Nitrogen</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">8</div>
+                <div className="elementSym">0</div>
+                <div className="elementName">Oxygen</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">9</div>
+                <div className="elementSym">F</div>
+                <div className="elementName">Fluorine</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">10</div>
+                <div className="elementSym">Ne</div>
+                <div className="elementName">Neon</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">11</div>
+                <div className="elementSym">Na</div>
+                <div className="elementName">Sodium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">12</div>
+                <div className="elementSym">Mg</div>
+                <div className="elementName">Magnesium</div>
+            </div>
+
             <div className="item">item_41</div>
             <div className="item">item_42</div>
             <div className="item">item_43</div>
@@ -55,34 +117,196 @@ const Table = () => {
             <div className="item">item_49</div>
             <div className="item">item_50</div>
             <div className="item">item_51</div>
-            <div className="item">item_52</div>
-            <div className="item">item_53</div>
-            <div className="item">item_54</div>
-            <div className="item">item_55</div>
-            <div className="item">item_56</div>
-            <div className="item">item_57</div>
-            <div className="item">item_58</div>
-            <div className="item">item_59</div>
-            <div className="item">item_60</div>
-            <div className="item">item_61</div>
-            <div className="item">item_62</div>
-            <div className="item">item_63</div>
-            <div className="item">item_64</div>
-            <div className="item">item_65</div>
-            <div className="item">item_66</div>
-            <div className="item">item_67</div>
-            <div className="item">item_68</div>
-            <div className="item">item_69</div>
-            <div className="item">item_70</div>
-            <div className="item">item_71</div>
-            <div className="item">item_72</div>
-            <div className="item">item_73</div>
-            <div className="item">item_74</div>
-            <div className="item">item_75</div>
-            <div className="item">item_76</div>
-            <div className="item">item_77</div>
-            <div className="item">item_78</div>
+
+            <div className="item">
+                <div className="atomicNumber">13</div>
+                <div className="elementSym">Al</div>
+                <div className="elementName">Aluminium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">14</div>
+                <div className="elementSym">Si</div>
+                <div className="elementName">Silicon</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">15</div>
+                <div className="elementSym">P</div>
+                <div className="elementName">Phosphorus</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">16</div>
+                <div className="elementSym">S</div>
+                <div className="elementName">Sulfur</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">17</div>
+                <div className="elementSym">Cl</div>
+                <div className="elementName">Chlorine</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">18</div>
+                <div className="elementSym">Ar</div>
+                <div className="elementName">Argon</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">19</div>
+                <div className="elementSym">K</div>
+                <div className="elementName">Potassium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">20</div>
+                <div className="elementSym">Ca</div>
+                <div className="elementName">Calcium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">21</div>
+                <div className="elementSym">Sc</div>
+                <div className="elementName">Scandium</div>
+            </div>
+
+            <div className="item"></div>
+
+            <div className="item">
+                <div className="atomicNumber">22</div>
+                <div className="elementSym">Ti</div>
+                <div className="elementName">Titanium</div>
+            </div>
+
+
+            <div className="item">
+                <div className="atomicNumber">23</div>
+                <div className="elementSym">V</div>
+                <div className="elementName">Vanadium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">24</div>
+                <div className="elementSym">Cr</div>
+                <div className="elementName">Chromium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">25</div>
+                <div className="elementSym">Mn</div>
+                <div className="elementName">Manganese</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">26</div>
+                <div className="elementSym">Fe</div>
+                <div className="elementName">Iron</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">27</div>
+                <div className="elementSym">Co</div>
+                <div className="elementName">Cobalt</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">28</div>
+                <div className="elementSym">Ni</div>
+                <div className="elementName">Nickel</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">29</div>
+                <div className="elementSym">Cu</div>
+                <div className="elementName">Copper</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">30</div>
+                <div className="elementSym">Zn</div>
+                <div className="elementName">Zinc</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">31</div>
+                <div className="elementSym">Ga</div>
+                <div className="elementName">Gallium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">32</div>
+                <div className="elementSym">Ge</div>
+                <div className="elementName">Germanium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">33</div>
+                <div className="elementSym">As</div>
+                <div className="elementName">Arsenic</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">34</div>
+                <div className="elementSym">Se</div>
+                <div className="elementName">Selenium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">35</div>
+                <div className="elementSym">Br</div>
+                <div className="elementName">Bromine</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">36</div>
+                <div className="elementSym">Kr</div>
+                <div className="elementName">Krypton</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">37</div>
+                <div className="elementSym">Rb</div>
+                <div className="elementName">Rubidium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">38</div>
+                <div className="elementSym">Sr</div>
+                <div className="elementName">Strontium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">39</div>
+                <div className="elementSym">Y</div>
+                <div className="elementName">Yttrium</div>
+            </div>
+
+
+            <div className="item"></div>
+
+
+            <div className="item">
+                <div className="atomicNumber">40</div>
+                <div className="elementSym">Zr</div>
+                <div className="elementName">Zirconium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">41</div>
+                <div className="elementSym">Nb</div>
+                <div className="elementName">Niobium</div>
+            </div>
+
+            <div className="item">
+                <div className="atomicNumber">42</div>
+                <div className="elementSym">Mo</div>
+                <div className="elementName">Molybdenum</div>
+            </div>
+
             <div className="item">item_79</div>
+            
             <div className="item">item_80</div>
             <div className="item">item_81</div>
             <div className="item">item_82</div>
@@ -195,10 +419,10 @@ const Table = () => {
             <div className="item">item_189</div>
             <div className="item">item_190</div>
         </section>
-    )
-}
+    );
+};
 
-export default Table
+export default Table;
 
 /*
 10 rows
